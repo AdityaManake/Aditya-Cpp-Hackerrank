b1b04cf35b33a56578e310e4daff93351bbcad84
Aditya Manake <adityapmanake@gmail.com>
2025-11-06 01:34:37 +0530

    RectangeArea.cpp

---
+++ RectangeArea.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+class Rectangle{
+    public:
+    int a,b;
+    void read_input()
+    {
+        cin>>a>>b;
+    }
+    void display()
+    {
+        cout<<a<<" "<<b<<endl;
+    }
+};
+class RectangleArea:public Rectangle
+{
+    public:
+    void display()
+    {
+        cout<<a*b;
+    }
+};
+
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
