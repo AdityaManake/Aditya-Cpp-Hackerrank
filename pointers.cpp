a54ae25a9ea82e8349c98d7a815e29d94adb6328
Aditya Manake <adityapmanake@gmail.com>
2025-11-06 01:28:13 +0530

    pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void update(int &a,int &b)
+{
+    int x=a , y=b;
+    a=x+y;
+    b=abs(x-y);
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    update(a,b);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
